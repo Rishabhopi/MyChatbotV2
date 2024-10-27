9c4e61db0bd042caa09d76b149b1c90f8a781217
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-27 07:35:02 +0530

    Update inline.py

--- Venom/modules/helpers/inline.py
+++ Venom/modules/helpers/inline.py
@@ -6,7 +6,7 @@ from Venom import VenomX
 
 DEV_OP = [
     [
-        InlineKeyboardButton(text="🍭ᴏᴡɴᴇʀ🍭", user_id=OWNER),
+        InlineKeyboardButton(text="**❍ 𝐎ᴡɴᴇʀ ❍**", user_id=OWNER),
         InlineKeyboardButton(text="✨ ꜱᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/{SUPPORT_GRP}"),
     ],
     [
